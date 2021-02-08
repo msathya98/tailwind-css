ad125a3e4646ef3ec1dea23381a710802baf5ac5
Sathya M <62427113+msathya98@users.noreply.github.com>
2021-02-08 19:46:37 +0530

    Home, about and menu pages of eggcelent page.

---
+++ Eggcelent/pages/index.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Hero from '../components/Hero';
+import Content from '../components/Content';
+
+const Home = () => {
+    return (
+        <>
+        <Hero />
+      <Content />
+
+        </>
+    )
+}
+
+export default Home
+
+
+
+
+
+
+// import React from 'react';
+
+// const index = () => {
+//     return (
+//         <div>
+
+//         </div>
+//     )
+// }
+
+// export default index
